518bcba0213e19734ec03dd7a69a3ea82be2bd2a
Fenniless <fenniless@gmail.com>
2019-02-09 12:06:06 -0500

    basic calculations class and 1 test case sample

--- CalculationsTest.java
+++ CalculationsTest.java
@@ -4,9 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by Joe Fennimore
- */
+
 public class CalculationsTest{
     private static volatile Calculations calculations = new Calculations();
     @Test
@@ -18,7 +16,7 @@ public class CalculationsTest{
         double expected = 10;
         double actual = Calculations.multiply(firstNum, secondNum);
         //Then
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual, 0);
     }
    
 }
