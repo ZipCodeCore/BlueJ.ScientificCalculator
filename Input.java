5ca3b7c580182897f0dd48a494813dcae27aaf9d
Mark Moll <mdmoll@gmail.com>
2019-02-09 16:41:27 -0500

    First draft of Input calculator

---
+++ Input.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class Input here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Input {
+    private static Calculations calc = new Calculations();
+    public static void main(String[] args) {
+        String input = "80 / 80";
+        String pattern = ("(\\d+)\\s?([+>\\-<=≠≥≤÷−*x·%/])\\s*(\\d+)");
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(input);
+        m.matches();
+        boolean found = p.matcher(input).lookingAt();
+        System.out.println("'" + pattern + "'" +
+                (found ? " matches '" : " doesn't match '") + input + "'");
+        String stringNum1 = m.group(1);
+        String stringNum2 = m.group(3);
+        String stringOp = m.group(2);
+        double unsignedFirstNum = Double.parseDouble(stringNum1);
+        double unsignedSecondNum = Double.parseDouble(stringNum2);
+        double result = 0.0;
+        switch(stringOp) {
+            case"+": result = calc.add(unsignedFirstNum,unsignedSecondNum);
+                break;
+            case"-":result = calc.subtract(unsignedFirstNum,unsignedSecondNum);
+                break;
+            case"−":result = calc.subtract(unsignedFirstNum,unsignedSecondNum);
+                break;
+            case"*":result = calc.multiply(unsignedFirstNum,unsignedSecondNum);
+                break;
+            case"/":result = calc.divide(unsignedFirstNum,unsignedSecondNum);
+                break;
+            case"÷":result = calc.divide(unsignedFirstNum,unsignedSecondNum);
+                break;
+            case"x":result = calc.multiply(unsignedFirstNum,unsignedSecondNum);
+                break;
+            case"·":result = calc.multiply(unsignedFirstNum,unsignedSecondNum);
+                break;
+        }
+        System.out.print(result);
+    }
+}
