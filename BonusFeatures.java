21ef6849b43e071ec9a7bceca772f5a6d95f8b03
Arcelia Calleja <arceliacalleja@gmail.com>
2019-02-10 10:03:58 -0500

    Added BonusFeature class stub

---
+++ BonusFeatures.java
@@ -0,0 +1,34 @@
+
+/**
+ * Bonus features for the calculator project
+ *
+ * @Arcelia Calleja
+ * @2/10/19
+ */
+public class BonusFeatures
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BonusFeatures
+     */
+    public BonusFeatures()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
