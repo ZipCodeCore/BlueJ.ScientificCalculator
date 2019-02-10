0147f4a1c6e554f20d03be38696e9eef83579ad1
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-02-10 14:30:40 -0500

    added changeMode feature to speak with calculator and displayMdde

--- Application.java
+++ Application.java
@@ -7,12 +7,38 @@ public class Application {
     public static void run() {
         IOConsole console = new IOConsole(System.in, System.out);
         Processor processor = new Processor();
+        String result = "";
+        String numConvert = "";
+
 
         boolean quit = false;
         
         // Hello 
         console.println("Hello");
         while(!quit) {
+            
+            console.println("Would you like to change the display?");
+            String input = console.getStringInput();
+            if (input.matches("yes")){
+                console.println("Would you like to view display options"); 
+                String viewDisplayinput = console.getStringInput();
+                if(viewDisplayinput.matches("yes")) {
+                    processor.displayMode("display");
+                } else {
+                    console.println("Enter Mode:");
+                    String mode = console.getStringInput();
+                    processor.displayMode(mode);
+                    console.println("Enter x");
+                    Double x = console.getDoubleInput();
+                    numConvert = processor.changeMode(mode, x);
+                    console.println(numConvert);
+                    // enter y
+                    console.println("Enter y");
+                    Double y = console.getDoubleInput();
+                    numConvert = processor.changeMode(mode, y);
+                    console.println(numConvert);
+                }
+            }
             // enter x
             console.println("Enter x");
             Double x = console.getDoubleInput();
@@ -23,15 +49,20 @@ public class Application {
             console.println("Enter y");
             Double y = console.getDoubleInput();
             
-            processor.arithmetic(operator, x, y);
+            result = processor.arithmetic(operator, x, y);
             // print result
+            console.log(result);
+            
+            console.println("Would you like to do more Math????");
+            console.println("Enter: yes to continue.");
+            console.println("Enter: no to quit.");
+
             // More math?
             String redo = console.getStringInput();
             if(redo.equals("no")) {
                 quit = true;
             }   
         }
-        console.println("bye!");
         // bye!
     }
 }
--- Processor.java
+++ Processor.java
@@ -1,79 +1,111 @@
 public class Processor {
-	
-	private static Calculator operations;
-	private static IOConsole console;
-	
-	public Processor() {
-		operations = new Calculator();
-		console = new IOConsole(System.in, System.out);
-	}
+    
+    private static Calculator operations;
+    private static IOConsole console;
+    
+    public Processor() {
+        operations = new Calculator();
+        console = new IOConsole(System.in, System.out);
+    }
 
-	public static void arithmetic(String operator, Double x, Double y) {
-		
-		if(operator.matches("binary|octal|decimal|hexadecimal")){
-			operations.switchDisplayMode(operator);
-		} else if(operator.equals("display")) {
-			operations.switchDisplayMode();
-		}
+    // display method that ask the user what they want to do and shows possible operations including display mode which switches to binary octal decimal hexadecimal
+    public static String changeMode(String mode, Double x, Double y) {
+    	String result = "";
+    	if(mode.matches("binary")){
+            Integer binaryX = (Integer) x;
+            Integer BinaryY = (Integer) y;
+            result += operations.toBinary(x);
+            result += operations.toBinary(y);
+        } else if(mode.matches("octal")){
+            Integer octalX = (Integer) x;
+            Integer octalY = (Integer) y;
+            result += operations.toOctal(x,y);
+            result += operations.toOctal(y);
+        } else if(mode.matches("decimal")){
+            Integer decimalX = (Integer) x;
+            Integer decimalY = (Integer) y;
+            result += operations.toDecimal(x);
+            result += operations.toDecimal(y);
+        } else if (mode.matches("hexadecimal")){
+            Integer hexadecimalX = (Integer) x;
+            Integer hexadecimalY = (Integer) y;
+            result += operations.toHexadecimal(x,y);
+            result += operations.toHexadecimal(y);
+        }
+        return result;
+    }
 
-		switch(operator.toLowerCase()) {
+    public static void displayMode(String operator) { 
+        if(operator.equals("display")) {
+            console.println("Display Mode:");
+            console.println("binary"); 
+            console.println("octal"); 
+            console.println("decimal"); 
+            console.println("hexadecimal");
+        }
+    }
+    
+    public static String arithmetic(String operator, Double x, Double y) {
+        String result = "";
+        switch(operator.toLowerCase()) {
 
-		case "+": operations.add(x,y);
-		break;
+        case "+": result+= operations.add(x,y);
+        break;
 
-		case "-": operations.subtract(x,y);
-		break;
-		
-		case "/": operations.divide(x,y);
-		break;
+        case "-": result+= operations.subtract(x,y);
+        break;
+        
+        case "/": result+= operations.divide(x,y);
+        break;
 
-		case "%": operations.remainder(x,y);
-		break;
+        case "%": result+= operations.remainder(x,y);
+        break;
 
-		case "*": operations.mulit(x,y);
-		break;
+        case "*": result+= operations.mulit(x,y);
+        break;
 
-		case "^": operations.exponentiation(x,y);
-		break;
-		
-		case "inverse": operations.inverse(x);
-		break;
+        case "^": result+= operations.exponentiation(x,y);
+        break;
+        
+        case "inverse": result+= operations.inverse(x);
+        break;
 
-		case "square": operations.square(x);
-		break;
-		
-		case "invert": operations.invert(x,y);
-		break;
+        case "square": result+= operations.square(x);
+        break;
+        
+        case "invert": result+= operations.invert(x,y);
+        break;
 
-		case "factorial": operations.factorial(x);
-		break;
-		
-		case "switchSign": operations.switchSign(x);
-		break;
+        case "factorial": result+= operations.factorial(x);
+        break;
+        
+        case "switchSign": result+= operations.switchSign(x);
+        break;
 
-		case "sqrt": operations.sqrt(x);
-		break;
-		
-		case "sine": operations.sine(x,y);
-		break;
+        case "sqrt": result+= operations.sqrt(x);
+        break;
+        
+        case "sine": result+= operations.sine(x,y);
+        break;
 
-		case "asin": operations.asin(x,y);
-		break;
+        case "asin": result+= operations.asin(x,y);
+        break;
 
-		case "cos": operations.cos(x,y);
-		break;
+        case "cos": result+= operations.cos(x,y);
+        break;
 
-		case "acos": operations.acos(x,y);
-		break;
+        case "acos": result+= operations.acos(x,y);
+        break;
 
-		case "tan": operations.tan(x,y);
-		break;
+        case "tan": result+= operations.tan(x,y);
+        break;
 
-		case "atan": operations.atan(x,y);
-		break;
+        case "atan": result+= operations.atan(x,y);
+        break;
 
-		case "quit":
-		break;
-		}
-	}
+        case "quit": result = "bye!";
+        break;
+        }
+        return result;
+    }
 }
